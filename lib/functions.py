27e29b8ec10f4427e8215c55c684abd15fc16c07
TheClutchC <claycarney7@gmail.com>
2023-04-28 22:30:22 -0400

    Python Functions Pt 1

--- lib/functions.py
+++ lib/functions.py
@@ -14,3 +14,32 @@ def add(num1, num2):
 
 def halve(number):
     pass
+
+# <--FUNCTIONS-->
+
+# JS vs. Python
+'''
+-Use 'def' instead of 'function' keyword for function blocks
+-snake_case instead of camelCase
+-use colon ':' instead of curly brackets to start code block
+-must indent code block to be executed, standard is 4 spaces
+-return statements work the same, just no semicolon
+-to end code block, new code is written at original indent level
+'''
+
+
+def my_function(param):
+    print("Running my_function")
+    return param + 1
+
+my_function_returned_value = my_function(1)
+print(my_function_returned_value)
+
+my_function_returned_value
+
+'''
+-Methods are just functions that belong to objects
+-They often act upon those objects when called
+'''
+
+# Start on Arguments tomorrow
